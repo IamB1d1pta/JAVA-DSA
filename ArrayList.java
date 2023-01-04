1c9d7634d338e22898fc3d6747aa38572e4f2d7e
Bidipta Das <95310218+IamB1d1pta@users.noreply.github.com>
2023-01-04 21:24:16 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class ArrayList
+{
+	public static void main(String[] args) {
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		list.add(1);
+		list.add(5);
+		list.arr(9);
+		System.out.print(list);
+	}
+}
