9c4f88ef18e5d2e71f4e3aff551e21bf2f7b33bd
Bidipta Das <95310218+IamB1d1pta@users.noreply.github.com>
2023-01-11 23:51:07 +0530

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,15 @@
+ class forl
+ {
+ 	int n=10;
+ 	for1(String a)
+ 	{
+ 		int n=5;
+ 	}
+ }
+ class forLoop
+ {
+ 	public static void main(String[] args) {
+ 		for1 h=new for1("Anubhab");
+ 		
+ 	}
+ }
