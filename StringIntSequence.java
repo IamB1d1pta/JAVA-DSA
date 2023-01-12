d2a6f4d2db8875f4d53e7db242033232a5cfb893
Bidipta Das <95310218+IamB1d1pta@users.noreply.github.com>
2023-01-12 21:26:06 +0530

    Add files via upload

---
+++ StringIntSequence.java
@@ -0,0 +1,13 @@
+//A1 B2 C3 D4 E5 F6 H7 I9 J10.....
+class StringIntSequence
+{
+	public static void main(String[] args) {
+		
+	int count=1;
+	for(char i='A';i<='Z';i++)
+	{
+		System.out.print(i+""+count+" ");
+		count++;
+	}
+}
+}
